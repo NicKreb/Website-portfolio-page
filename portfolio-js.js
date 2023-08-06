8f5e07a72d02416a227d6c431bd42a5aef5866eb
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-08-06 17:51:40 -0500

    Update portfolio-js.js
    started working on animation for the color tabs to fill the screen gradually.

--- portfolio-js.js
+++ portfolio-js.js
@@ -1,1 +1,6 @@
+document.getElementById("magenta").addEventListener("click", fillScreen);
 
+function fillScreen() {
+    // document.getElementById("magenta").style.height = "200%"
+    // document.getElementById("magenta").style.width = "150%";
+}
