5c0a0ee1e3073f5a9f8f2de4320def5a3682785a
Nick H <113550184+NicKreb@users.noreply.github.com>
2023-08-11 22:38:33 -0500

    Update and rename portfolio-js.js to darkmode.js

--- portfolio-js.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("magenta").addEventListener("click", fillScreen);
-
-function fillScreen() {
-    // document.getElementById("magenta").style.height = "200%"
-    // document.getElementById("magenta").style.width = "150%";
-}
